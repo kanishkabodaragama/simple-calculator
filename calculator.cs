5430ba1bb7f88f939984a9274e0b1153ad74b3e8
aahsbadhikari <67773789+aahsbadhikari@users.noreply.github.com>
2021-10-22 00:07:30 +0530

    Update calculator.cs

--- calculator.cs
+++ calculator.cs
@@ -39,20 +39,24 @@ namespace CalculatorApp {
                     break;  
             }  
             Console.WriteLine("The result is {0}", result);  
-            Console.ReadKey();  
+            Console.ReadKey();
         }  
+        //Addition
         public static int Addition(int input_1, int input_2) {  
             int result = input_1 + input_2;  
             return result;  
         }  
+        //Substraction
         public static int Subtraction(int input_1, int input_2) {  
             int result = input_1 + input_2;  
             return result;  
         }    
+        //Multiplication
         public static int Multiplication(int input_1, int input_2) {  
             int result = input_1 + input_2;  
             return result;  
         } 
+        //Division
         public static int Division(int input_1, int input_2) {  
             int result = input_1 + input_2;  
             return result;  
